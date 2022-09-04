c09c7fdaf0f94582642d488dc26f1efb772a81af
Igor <76621236+Cl1nt0n@users.noreply.github.com>
2022-09-04 14:29:07 +0300

    классы базы

---
+++ It-hosting 2.0 — копия/It-hosting 2.0/Model/DBModels/Branch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace It_hosting_2._0.Models.DBModels
+{
+    public partial class Branch
+    {
+        public Branch()
+        {
+            Commits = new HashSet<Commit>();
+            PullRequestFromBranches = new HashSet<PullRequest>();
+            PullRequestToBranches = new HashSet<PullRequest>();
+            Repositories = new HashSet<Repository>();
+        }
+
+        public int Id { get; set; }
+        public string? File { get; set; }
+        public int RepositoryId { get; set; }
+
+        public virtual Repository Repository { get; set; } = null!;
+        public virtual ICollection<Commit> Commits { get; set; }
+        public virtual ICollection<PullRequest> PullRequestFromBranches { get; set; }
+        public virtual ICollection<PullRequest> PullRequestToBranches { get; set; }
+        public virtual ICollection<Repository> Repositories { get; set; }
+    }
+}
---
+++ It-hosting 2.0 — копия/It-hosting 2.0/ViewModel/SignUpViewModel.cs
@@ -0,0 +1,113 @@
+﻿using It_hosting_2._0.Model.Tools;
+using It_hosting_2._0.Models.DBModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace It_hosting_2._0.ViewModel
+{
+    internal class SignUpViewModel
+    {
+        private CommandTemplate _addUserCommand;
+
+
+        private string _userName;
+        private string _login;
+        private string _password;
+        private string _confrimPassword;
+
+        public string UserName
+        {
+            get => _userName;
+            set
+            {
+                _userName = value;
+                OnPropertyChanged(nameof(UserName));
+            }
+        }
+
+        public string Login
+        {
+            get => _login;
+            set
+            {
+                _login = value;
+                OnPropertyChanged(nameof(Login));
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        public string ConfrimPassword
+        {
+            get => _confrimPassword;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(ConfrimPassword));
+            }
+        }
+
+        #region Команды
+
+        public CommandTemplate AddUserCommand
+        {
+            get
+            {
+                if (_addUserCommand == null && string.IsNullOrWhiteSpace(_login) && string.IsNullOrWhiteSpace(_userName)
+                    && string.IsNullOrWhiteSpace(_password))
+                {
+                    if (_password == _confrimPassword)
+                    {
+                        _addUserCommand = new CommandTemplate(obj =>
+                        {
+                            AddUser();
+                        });
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Поля не должны быть пустыми !");
+                    return null;
+                }
+
+                return _addUserCommand;
+            }
+        }
+
+        #endregion
+
+        public void AddUser()
+        {
+            using (ithostingContext db = new ithostingContext())
+            {
+                User user = new User();
+
+                user = new User { Login = _login, Password = _password, Nickname = _userName };
+                db.Users.Add(user);
+                db.SaveChanges();
+
+                MessageBox.Show("Пользователь добавлен успешно !");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
